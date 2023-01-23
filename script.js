ae9e8cf59d6a0db88e86044cd3fcc0c72a4254d1
Maha Abdelnabi <mahaomar@gmail.com>
2023-01-22 22:33:21 -0500

    writing new js code

--- script.js
+++ script.js
@@ -93,3 +93,72 @@ let theQuestions = [
     correct: "Text3",
   },
 ];
+
+
+//select elements
+var quizApp = document.getElementById("quiz-app");
+var quizArea = document.querySelector(".quiz-area");
+//h2 question
+var questionEl = document.getElementById("question");
+//li all the answers
+var answerArea = document.querySelectorAll(".answer");
+var countSpan = document.querySelector(".count span");
+//li each answer
+var answer_1 = document.getElementById("answer1");
+var answer_2 = document.getElementById("answer2");
+var answer_3 = document.getElementById("answer3");
+var answer_4 = document.getElementById("answer4");
+//button
+var submitButton = document.querySelector(".submit-button");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  //enter to each index of an question array
+  const currentQuizData = theQuestions[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  answer_1.innerText = currentQuizData.Text1;
+  answer_2.innerText = currentQuizData.Text2;
+  answer_3.innerText = currentQuizData.Text3;
+  answer_4.innerText = currentQuizData.Text4;
+}
+
+function deselectAnswers() {
+   answerArea.forEach(function (answerEl) {
+     if ((answerEl.checked = false)) return answerEl;
+   });
+}
+
+function getSelected() {
+  let answer;
+  answerArea.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+submitButton.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === theQuestions[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+
+    if (currentQuiz < theQuestions.length) {
+      loadQuiz();
+    } else {
+      quizApp.innerHTML = `<h2> you answer ${score}/ ${theQuestions.length} quesions correctly </h2>
+  
+  <button onclick = "location.relaoad()">reload</button>
+  `;
+    }
+  }
+});
